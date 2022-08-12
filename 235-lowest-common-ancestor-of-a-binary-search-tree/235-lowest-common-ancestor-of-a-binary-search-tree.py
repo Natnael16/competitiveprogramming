a9d2ec97a40caeb422cabc4b379e3cb65e0b7a6f
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-08-12 16:48:54 +0300

    Time: 86 ms (89.06%), Space: 18.7 MB (68.62%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -17,12 +17,7 @@ class Solution:
             
         return TreeNode(val = helper(root , p ,q ))
             
-            #i will check if p <= node.val and q>= node.val or p >= node.val and q <= node.val
-                # if true return node.val
-                
-            # if both > node.val: return  go right
-            #if both < node.val: return go left
-            
+          
             
             
           
