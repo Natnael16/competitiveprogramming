907ff187176882b208727b420f04c2c8e7112744
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-08-18 23:13:40 +0300

    Time: 95 ms (48.49%), Space: 13.9 MB (74.19%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,13 +1,22 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         #top down 
-        if len(cost) == 1: return cost[0]
-        cost.append(0)
-        @lru_cache(None)
-        def dp(take): 
-            if take >= len(cost) - 1:
-                return 0
-            return cost[take] + min(dp(take + 1) , dp(take + 2))
-        return dp(-1)
+        # if len(cost) == 1: return cost[0]
+        # cost.append(0)
+        # @lru_cache(None)
+        # def dp(take): 
+        #     if take >= len(cost) - 1:
+        #         return 0
+        #     return cost[take] + min(dp(take + 1) , dp(take + 2))
+        # return dp(-1)
+        #bottom up
+        
+        top = 0
+        last = cost[-1]
+        for i in range(len(cost) - 2 , -1 , -1):
+            new = cost[i] + min(last , top)
+            top = last
+            last = new
+        return min(last , top)
             
             
