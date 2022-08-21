2460bf4ea6c26206e26d07821921120a1bf9cde2
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-08-21 13:29:58 +0300

    Time: 2069 ms (65.82%), Space: 28.4 MB (84.50%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -10,11 +10,18 @@ class Solution:
                     out of bound return 0
         '''
 
-        @lru_cache(None)
-        def dp(ptr, isEven):
-            if ptr == len(nums): return 0
-            num = nums[ptr] if isEven else -nums[ptr]
+#         @lru_cache(None)
+#         def dp(ptr, isEven):
+#             if ptr == len(nums): return 0
+#             num = nums[ptr] if isEven else -nums[ptr]
             
-            return max(num + dp(ptr + 1 , not isEven) , dp(ptr + 1 , isEven))
-        return dp(0 ,True)
+#             return max(num + dp(ptr + 1 , not isEven) , dp(ptr + 1 , isEven))
+#         return dp(0 ,True)
         
+        #bottom up
+        sumOdd , sumEven = 0 , 0
+        
+        for i in range(len(nums) - 1 ,-1, -1):
+            sumOdd = max(sumEven-nums[i] , sumOdd)
+            sumEven = max(sumOdd + nums[i] , sumEven)
+        return max(sumOdd , sumEven)
