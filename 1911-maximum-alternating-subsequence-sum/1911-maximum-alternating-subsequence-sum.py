3c55aa7eb308092d3ba7250ab7e9053cf97d298b
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-08-21 13:19:13 +0300

    Time: 2665 ms (44.59%), Space: 411.2 MB (5.10%) - LeetHub

---
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxAlternatingSum(self, nums: List[int]) -> int:
+        '''[4 , 2 ,5, 3]
+        
+        4 , odd , sums even and odd
+            2 , 5 , 3
+            
+            
+        base case if len == 1 return even - odd
+                    out of bound return 0
+        '''
+
+        @lru_cache(None)
+        def dp(ptr, isEven):
+            if ptr == len(nums): return 0
+            num = nums[ptr] if isEven else -nums[ptr]
+            
+            return max(num + dp(ptr + 1 , not isEven) , dp(ptr + 1 , isEven))
+        return dp(0 ,True)
+        
