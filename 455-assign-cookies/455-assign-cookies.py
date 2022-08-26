58b315cf51ea252fab431404b08996c84fb966ca
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-08-26 15:01:06 +0300

    Time: 237 ms (63.63%), Space: 15.7 MB (81.81%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.py
+++ 455-assign-cookies/455-assign-cookies.py
@@ -2,7 +2,6 @@ class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g.sort()
         s.sort()
-        
         count = 0
         j = 0
         i = 0
@@ -10,9 +9,7 @@ class Solution:
             if g[i] > s[j]:
                 j += 1
             else:
-                j += 1
-                count += 1
-                i += 1
+                j += 1;count += 1;i += 1
         return count
                 
                 
