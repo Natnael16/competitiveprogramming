91fed0a48a544cd2405c5d81fe4b8fe08a1eb610
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-09-21 10:48:06 +0300

    Time: 7869 ms (23.15%), Space: 39.2 MB (50.45%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.py
@@ -1,8 +1,14 @@
 class Solution(object):
-    def findLength(self, A, B):
-        memo = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
-        for i in range(len(A) - 1, -1, -1):
-            for j in range(len(B) - 1, -1, -1):
-                if A[i] == B[j]:
-                    memo[i][j] = memo[i + 1][j + 1] + 1
-        return max(max(row) for row in memo)
+    def findLength(self, nums1, nums2):
+        
+        
+        dp = [[0] * (len(nums2) + 1) for _ in range(len(nums1) + 1)]
+        for a in range(len(nums1) - 1, -1, -1):
+            for b in range(len(nums2) - 1, -1, -1):
+                if nums1[a] == nums2[b]:
+                    dp[a][b] = dp[a+1][b+1] + 1
+        ans = -inf
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                ans = max(ans , dp[i][j])
+        return ans
