1097a4e7c5ae3ce893be205a36199e3344f3c373
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-09-27 21:29:33 +0300

    Time: 176 ms (29.81%), Space: 14.3 MB (80.35%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        [2,4,-2,3]
+        ans = max(nums)
+        maxi , mini = nums[-1] , nums[-1]
+        # set max and min from self and num * maxi and num  * mini
+        
+        for i in range(len(nums) - 2, -1,-1):
+            temp = maxi
+            maxi = max(nums[i], nums[i]* maxi,nums[i]*mini)
+            mini = min(nums[i], nums[i]* temp,nums[i]*mini)
+            
+            ans = max(maxi, ans)
+        return ans
+        
+#         if len(nums) == 1: return nums[0]
+#         @lru_cache(None)
+#         def dp(ind, flag):
+#             if ind == len(nums): return 1
+            
+#             if flag:
+#                 if nums[ind] < 0:
+#                     return min( nums[ind], nums[ind] * dp(ind + 1, True ),dp(ind + 1, True ))
+#                 else: return min(nums[ind] ,nums[ind] * dp(ind + 1, False),dp(ind + 1, False))
+#             else:
+#                 if nums[ind] < 0:
+#                     return max( nums[ind],nums[ind] * dp(ind + 1, True ),dp(ind + 1, True ))
+#                 else: return max(nums[ind],nums[ind] * dp(ind + 1, False),dp(ind + 1, False))
+#         return dp(0, False) solution has some bug
+            
+
+    
+        
