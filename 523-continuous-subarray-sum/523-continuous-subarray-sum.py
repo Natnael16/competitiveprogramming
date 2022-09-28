3fc1c68a352bcc0bdfe30f5df8866cd9bb9c693e
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-09-28 13:24:29 +0300

    Time: 2590 ms (10.40%), Space: 33.8 MB (16.75%) - LeetHub

---
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        """
+        The key is calculating the prifix sum and recording their remainder, if reaminder is repeated we know we have multiple of k some where.
+        """
+        remainder = {0 : -1} 
+        psum = 0
+        for ind , num in enumerate(nums):
+            psum += num
+            modRes = psum % k
+            if modRes in remainder:
+                if ind - remainder[modRes] > 1: return True
+            else:
+                remainder[modRes] = ind 
+        return False
+        
