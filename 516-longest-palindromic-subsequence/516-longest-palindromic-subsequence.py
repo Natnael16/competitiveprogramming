fa4ffd2471c90d1b5388ae761d1a17f214f57d01
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-09-29 15:28:11 +0300

    Time: 5134 ms (23.59%), Space: 39.1 MB (48.13%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -2,7 +2,7 @@ class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
 #         r = s[::-1]
         str_len = len(s)
-        
+#         top_down approach
 #         @lru_cache(None)
 #         def dp(p1, p2):
 #             if p1 >= str_len or p2 >= str_len:
@@ -13,19 +13,12 @@ class Solution:
 #                 return max(dp(p1 + 1, p2), dp(p1, p2 + 1))
 #         return dp(0,0)
         
-        
-             #      b a b b b
-             #    0 0 0 0 0 0 
-             # b  0 1 0 1 1 1
-             # b  0 1 1 1 2 1
-             # b  0 1 1 2 2 3
-             # a  0 0 1 2 1 0
-             # b  0 0 0 0 0 0
-        r = s[::-1]
+    
+        #bottom up approach        
         dp = [[0] * (str_len + 1) for _ in range(str_len + 1)]
         for front in range(1, str_len + 1):
             for back in range(1 , str_len + 1):
-                if s[front - 1] == r[back - 1]:
+                if s[front - 1] == s[str_len - (back)]:
                     dp[front][back] = dp[front- 1][back - 1] + 1
                 else:
                     dp[front][back] = max(dp[front- 1][back], dp[front][back - 1])
