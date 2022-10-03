b6c6ebb3dfbe56bf0954d11b6de8d27a430023a9
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-10-03 23:18:11 +0300

    Time: 784 ms (53.63%), Space: 21.3 MB (9.77%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,15 +1,14 @@
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        @lru_cache(1000)
-        def hel(n, target):
-            if target < n or target > n*k:
-                return 0 
-            if n == 1:
-                return 1
-            
-            s = 0 
-            for i in range(1, k+1): 
-                s += hel(n-1, target-i) 
-            return s
-            
-        return int(hel(n, target)) % (10**9 + 7)
+        nums = [i for i in range(1,k+1)]
+        @lru_cache(None)
+        def dp(tar , freq):
+            if tar <=0 or freq == 0:
+                if tar == 0 and freq == 0:
+                    return 1
+                return 0
+            tot = 0
+            for num in nums:
+                tot += dp(tar - num, freq -1)
+            return tot
+        return dp(target,n) % (10**9 + 7)
