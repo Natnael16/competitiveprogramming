ab872d80e9dd4b9f51ac85cdc65ff1f1c53d02a4
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-10-03 21:55:21 +0300

    Time: 127 ms (5.11%), Space: 14 MB (69.87%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -2,17 +2,37 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         # catsandog
         #   |
-        wordset = set(wordDict)
-        @lru_cache(None)
-        def dp(ind):
-            if ind == len(s): return True
+        #top_down
+        
+#         wordset = set(wordDict)
+#         @lru_cache(None)
+#         def dp(ind):
+#             if ind == len(s): return True
             
-            word = []
-            flag = False
-            for i in range(ind,len(s)):
-                word.append(s[i])
-                if "".join(word) in wordset:
-                    flag = flag or dp(i + 1)
-            return flag
-        return dp(0)
+#             word = []
+#             flag = False
+#             for i in range(ind,len(s)):
+#                 word.append(s[i])
+#                 if "".join(word) in wordset:
+#                     flag = flag or dp(i + 1)
+#             return flag
+#         return dp(0)
+
+        n = len(s)
+        wordset = set(wordDict)
+        dp = [False] * (n + 1)
+        dp[-1] = True
+        
+        for i in range(n-1,-1,-1):
+            queue = deque([])
+            for j in range(i,-1,-1):
+                queue.appendleft(s[j])
+                if "".join(queue) in wordset and dp[i + 1]:
+                    dp[j] = True
+        return dp[0]
+        
+        
+        
+    
+    
         
