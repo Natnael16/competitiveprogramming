95b5e6a3f0e251bd925d136ed79b59f38a60949e
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-10-05 21:58:29 +0300

    Time: 68 ms (33.97%), Space: 13.7 MB (99.84%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -1,16 +1,23 @@
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         
-        numsset = set(nums)
-        self.ans = "" # assuming you submit it in leetcode 
-        def backTrack(s,target_len):
-            if len(s) == target_len:
-                if s not in numsset:
-                    self.ans = s
-                return
-        
-            backTrack(s+"0",target_len)
-            backTrack(s+"1",target_len)
+#         numsset = set(nums)
+#         self.ans = "" # assuming you submit it in leetcode 
+#         def backTrack(s,target_len):
+#             if len(s) == target_len:
+#                 if s not in numsset:
+#                     self.ans = s
+#                 return
+                                                                    
+#             backTrack(s+"0",target_len)
+#             backTrack(s+"1",target_len)
             
-        backTrack("",len(nums))
-        return self.ans
+#         backTrack("",len(nums))
+#         return self.ans
+        ints = set()
+        for binary in nums:
+            ints.add(int(binary,base=2))
+        for num in range(0,2**len(nums)): #itertates maximum 16 times
+            if num not in ints:
+                return ("000000000000000" + str(bin(num)).split("b")[1])[-len(nums):]
+            
