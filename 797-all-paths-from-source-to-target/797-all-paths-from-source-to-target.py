db298cdb48bca7f0e62243de5efa8e7ce476a4d1
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-10-07 10:52:39 +0300

    Time: 227 ms (20.89%), Space: 17.4 MB (6.90%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -1,13 +1,14 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        self.ans = []
-        def backtrack(node , path):
-            path.append(node)
+        @lru_cache(None)
+        def backtrack(node):
             if node == len(graph) - 1: 
-                self.ans.append(list(path))
-            
+                return [[node]]
+            ways = []
             for neigh in graph[node]:
-                backtrack(neigh,path)
-            path.pop()
-        backtrack(0,[])
-        return self.ans
+                res = backtrack(neigh)
+                for r in res:
+                    ways.append([node] + r)
+            return ways
+        return backtrack(0)
+       
