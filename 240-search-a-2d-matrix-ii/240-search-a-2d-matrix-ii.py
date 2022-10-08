eb3fa8b7a649908d3c5de59ce9d6cc15570a531b
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-10-08 11:02:21 +0300

    Time: 248 ms (70.49%), Space: 20.4 MB (83.48%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -1,18 +1,31 @@
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         def bSearch(arr , tar , hi ,lo):
+#             while lo <= hi:
+#                 mid = (hi + lo)//2
+                
+#                 if arr[mid] > tar:
+#                     hi = mid - 1
+#                 elif arr[mid] < tar:
+#                     lo = mid + 1
+#                 else:
+#                     return mid
+#             return -1
+#         for row in matrix:
+#             found = bSearch(row,target,len(row)-1,0)
+#             if found != -1:
+#                 return True
+#         return False
+# if less than cur go left elif greater go down
+
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        def bSearch(arr , tar , hi ,lo):
-            while lo <= hi:
-                mid = (hi + lo)//2
-                
-                if arr[mid] > tar:
-                    hi = mid - 1
-                elif arr[mid] < tar:
-                    lo = mid + 1
-                else:
-                    return mid
-            return -1
-        for row in matrix:
-            found = bSearch(row,target,len(row)-1,0)
-            if found != -1:
-                return True
+        n, m = len(matrix), len(matrix[0])
+        row, col = 0,m - 1
+        while 0 <= row < n and 0 <= col < m:
+            if matrix[row][col] > target:
+                col -= 1
+            elif matrix[row][col] < target:
+                row += 1
+            else: return True
         return False
