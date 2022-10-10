1e3a839eab3cb9454399d1ae5000bb4b92eeb156
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-10-10 21:30:13 +0300

    Time: 1065 ms (87.79%), Space: 128.6 MB (19.26%) - LeetHub

--- 565-array-nesting/565-array-nesting.py
+++ 565-array-nesting/565-array-nesting.py
@@ -1,16 +1,18 @@
 class Solution:
     def arrayNesting(self, nums: List[int]) -> int:
-        seen = set()
+        
         ans = -1
         def dfs(ind , count):
             nonlocal ans
-            if ind in seen:
+            if nums[ind] == -1:
                 ans = max(ans, count)
                 return 
-            seen.add(ind)
+            
             next_ind = nums[ind]
+            nums[ind] = -1
             dfs(next_ind ,  count + 1)
         for i in range(len(nums)):
-            dfs(i,0)
+            if nums[i] != -1:
+                dfs(i,0)
         return ans
             
