5224ebb80235e41e3ca6ed1563291a00d8db41e9
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-10-23 21:33:51 +0300

    Time: 105 ms (38.82%), Space: 14 MB (35.00%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,30 +1,16 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        valueSymbol = {
-                1   : 'I',
-                4   : 'IV',
-                5   : 'V',
-                9   : 'IX',
-                10  : 'X',
-                40  : 'XL',
-                50  : 'L',
-                90  : 'XC',
-                100 : 'C',
-                400 : 'CD',
-                500 : 'D',
-                900 : 'CM',
-                1000: 'M',
-        }
-        values = sorted(valueSymbol.keys(),reverse = True)
+        symbols = ['I','IV','V','IX','X','XL','L','XC','C','CD','D','CM' ,'M']
+        values =  [1,4,5,9,10,40,50,90,100,400,500,900 ,1000]
         roman = ''
+        back = len(values) - 1
         while num > 0:
-            for value in values:
-                if value <= num:
+            for index in range(back,-1,-1):
+                if values[index] <= num:
                     break
-            roman += valueSymbol[value]
-            num = num - value
-           
-            
+            roman += symbols[index]
+            num = num - values[index]
+     
         return roman
             
             
