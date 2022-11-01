8418f68c38f6ea026a7ccfba377c8a1b02e9a985
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-11-01 17:31:00 +0300

    Time: 139 ms (68.74%), Space: 15.3 MB (12.46%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.py
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.py
@@ -1,11 +1,21 @@
 class Solution:
     def countBattleships(self, board: List[List[str]]) -> int:
+        def dfs(row, col , rows, cols, visited, board):
+            visited.add((row,col))
+            if row + 1 < rows and board[row + 1][col] == "X":
+                dfs(row + 1, col,rows,cols,visited,board)
+            elif col + 1 < cols and board[row][col + 1] == "X":
+                dfs(row, col + 1,rows,cols,visited,board)
+            
+        def main(board):
             count = 0
+            visited = set()
             rows , cols = len(board), len(board[0])
-    
             for row in range(rows):
                 for col in range(cols):
-                    if board[row][col] != ".":
-                        if (row - 1 < 0 or board[row - 1][col] == ".")  and (col - 1 < 0 or board[row][col - 1] == "."):
-                            count += 1
+                    
+                    if board[row][col] != "." and (row,col) not in visited:
+                        dfs(row, col,rows, cols , visited, board)
+                        count += 1
             return count
+        return main(board)
