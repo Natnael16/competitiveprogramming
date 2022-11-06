5d2d875fa40c9eb2651da7ed2046e88a0ebf0d43
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-11-06 21:39:20 +0300

    Time: 42 ms (86.90%), Space: 14 MB (39.29%) - LeetHub

--- 0899-orderly-queue/0899-orderly-queue.py
+++ 0899-orderly-queue/0899-orderly-queue.py
@@ -6,10 +6,8 @@ class Solution:
             mini_str = deque(s)
             cur_str = deque(s)
             for i in range(len(s)):
-                
                 first = cur_str.popleft()
                 cur_str.append(first)
-                
                 if cur_str < mini_str:
                     mini_str = cur_str.copy()
             return "".join(mini_str)
