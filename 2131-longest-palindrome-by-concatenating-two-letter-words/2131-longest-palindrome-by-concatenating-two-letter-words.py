d319a6815b19379e4f0aa49d19d1de5ee6333ae5
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-11-07 21:32:21 +0300

    Time: 2820 ms (60.05%), Space: 38.1 MB (95.79%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -9,18 +9,17 @@ class Solution:
                 if word == reverse:
                     if not words_count[word] % 2:
                         pal_count += words_count[word] * 2
-                        words_count[word] = 0
                     else:
                         pal_count += (words_count[word] - 1) * 2
-                        odds += 1
-                        words_count[word] = 0
+                        odds = 2
+                    words_count[word] = 0
                 
                 elif words_count[reverse] > 0:
                     words_count[word] -= 1
                     pal_count += 4
                     words_count[reverse] -= 1
        
-        return pal_count + 2 if odds else pal_count
+        return pal_count + odds
             
             
             
