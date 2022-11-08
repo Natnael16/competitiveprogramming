b1090df915b9d18303add5b85e01acd6a1ec40e4
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-11-08 14:05:01 +0300

    Time: 338 ms (20.35%), Space: 15.5 MB (99.89%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        number = 0
+        shift = 1
+        neg_count = 0
+        for _ in range(32):
+            one_count, zero_count = 0,0
+            for num in nums:
+                if num < 0:
+                    neg_count += 1
+                if num & shift:
+                    one_count += 1
+                else:
+                    zero_count += 1
+            
+            if one_count % 3:
+                number |= shift
+            shift <<= 1
+            
+        return number - 2**32 if neg_count % 3 else number
+    
+            
+          
+        
