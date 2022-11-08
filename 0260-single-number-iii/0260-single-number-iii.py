af1276554ac2e9d840c69b78dce0e71fa854b73c
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-11-08 14:47:10 +0300

    Time: 137 ms (41.20%), Space: 14.7 MB (100.00%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,9 +1,10 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> List[int]:
+    def getXor(self,nums):
         xor = 0
         for num in nums:
             xor ^= num
-        
+        return xor
+    def getfirstOnPos(self,xor):
         get_bit = 1
         count = 0
         for _ in range(32):
@@ -11,14 +12,21 @@ class Solution:
                 break
             count += 1
             get_bit <<=1
+        return 1 << count
+        
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        
+        xor = self.getXor(nums)
+        check_bit = self.getfirstOnPos(xor)
         
         num1, num2 = 0, 0
-        check_bit = 1 << count
+        
         for num in nums:
             if check_bit & num:
                 num1 ^= num
             else:
                 num2 ^= num
+                
         return [num1,num2]
             
         
