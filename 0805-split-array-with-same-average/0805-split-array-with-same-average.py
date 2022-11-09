486be56420df97afe9db85bdd041ba24f3a6c9db
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-11-09 09:48:19 +0300

    Time: 2088 ms (31.38%), Space: 394.3 MB (5.32%) - LeetHub

---
+++ 0805-split-array-with-same-average/0805-split-array-with-same-average.py
@@ -0,0 +1,22 @@
+class Solution:
+    def splitArraySameAverage(self, nums: List[int]) -> bool:
+
+        nums_len = len(nums)
+        total = sum(nums)
+        @cache
+        def canSplit(index, sum1, length1):
+            
+            if length1 and nums_len - length1:
+                if sum1/length1 == (total - sum1)/(nums_len - length1):
+                    return True
+            if index >= nums_len:
+                return False
+            
+            return canSplit(index + 1, sum1 + nums[index], length1 + 1) or canSplit(index + 1, sum1,length1)
+        for i in range(1,nums_len):
+            if not total * i % nums_len:
+                return canSplit(0,0,0)
+        return False
+            
+                
+        
