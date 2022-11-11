00729825dd233821ac46a4b116c6aba2aad8293a
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-11-12 01:21:06 +0300

    Time: 67 ms (92.55%), Space: 14.6 MB (78.48%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        length = len(nums)
+        i,j= 0,1   
+        if length == 1 or length == 0:
+            return
+        maximum_val = max(nums) + 1
+        while j != length:
+            if nums[i] == nums[j]:
+                nums[i]= maximum_val
+            i += 1
+            j += 1
+        duplicates = length - nums.count(maximum_val)
+        nums.sort()
+        return duplicates
+        
