7e70b36e99f63e86136396504ba964d46940fbc4
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-11-17 11:39:39 +0300

    Time: 50 ms (94.22%), Space: 13.9 MB (83.96%) - LeetHub

---
+++ 2079-watering-plants/2079-watering-plants.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        cur_capacity = capacity
+        steps = 0
+        cur_pos = -1
+        while cur_pos!= len(plants) - 1 :
+            if cur_pos + 1 < len(plants) and plants[cur_pos + 1] <= cur_capacity:
+                steps += 1
+                cur_pos += 1
+                cur_capacity -= plants[cur_pos]
+            else:
+                steps += (cur_pos + 1)*2
+                cur_capacity = capacity
+                
+        return steps
