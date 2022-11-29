923da0b5267e08c42d41cf46ddebe0b030c2ebf7
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-11-29 23:26:44 +0300

    Time: 3980 ms (20.14%), Space: 13.8 MB (78.98%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -1,17 +1,15 @@
 class Solution:
-    def countArrangement(self, n: int) -> int:
-       
-        def backtrack(index,path):
-            
-            if index > n:
-                return 1
-            count = 0
-            for num in range(1,n + 1):
-                if num not in path:
-                    if num % index == 0 or index % num == 0:
-                        path.add(num)
-                        count += backtrack(index + 1, path)
-                        path.remove(num)
-            return count
-        return backtrack(1,set())
+    def countArrangement(self, n: int,index=1, path = set()) -> int:  
+        if index > n:
+            return 1
+        count = 0
+        for num in range(1,n + 1):
+            if num not in path:
+                if num % index == 0 or index % num == 0:
+                    path.add(num)
+                    count += self.countArrangement(n,index + 1,path)
+                    path.remove(num)
+        return count
+        
+    
          
