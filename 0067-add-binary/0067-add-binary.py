ba231f4dea9b8d352393166113a830f6fe78ddb1
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-12-02 13:17:16 +0300

    Time: 77 ms (17.77%), Space: 14 MB (25.99%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,11 +1,31 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        int_a = int(a , 2)
-        int_b = int(b , 2)
-        carry = (int_a & int_b) << 1
-        xor = (int_a ^ int_b)
-        while carry != 0:
-            carry, xor = (carry & xor) << 1, (xor ^ carry)
-        return bin(xor).split("b")[1]
+        #make a smaller length
+        if len(b) < len(a):
+            a, b = b, a
+        lendiff = len(b) - len(a)
+        a = ("0" * lendiff) + a # make them equal
+        
+        ans = []
+        carry = 0
+        for i in range(len(a) - 1, -1, -1):
+            carry += int(a[i]) + int(b[i])
+            if carry == 0 or carry == 1:
+                ans.append(str(carry))
+                carry = 0
+            elif carry == 2:
+                ans.append("0")
+                carry = 1
+            elif carry == 3:
+                ans.append("1")
+                carry = 1
+        if carry == 1:
+            ans.append("1")
+        return "".join(ans[::-1])
+            
+                
+            
+            
+            
     
     
