63cecc7507893ac77432797fe94f21fa66cd34f5
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-12-02 12:35:49 +0300

    Time: 524 ms (6.33%), Space: 15.6 MB (35.28%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = Counter(nums)
-        for c in count:
-            if count[c] > len(nums)//2:
-                return c
-        
+        ans = 0
+        shift = 1
+        for _ in range(32):
+            count0 , count1 = 0, 0
+            for num in nums:
+                if shift & num:
+                    count1 += 1
+                else:
+                    count0 += 1
+            if count1 > count0:
+                ans |= shift
+            shift <<= 1
+        return  ans - (2**32) if ans > 5*10**4 else ans
                 
