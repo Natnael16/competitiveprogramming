30d99e9903337899cf8b45fe87e639e203894f69
Natnael Tadele <91603089+Natnael16@users.noreply.github.com>
2022-12-09 21:45:29 +0300

    Time: 48 ms (91.07%), Space: 14.8 MB (21.28%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -3,9 +3,9 @@ class Solution:
         stack = [-1]
         max_len = 0
         for ind in range(len(s)):
-            if not stack or s[ind] == "(":
+            if stack[-1] == -1 or s[ind] == "(":
                 stack.append(ind)
-            elif s[stack[-1]] == "(" and stack[-1] != -1:
+            elif s[stack[-1]] == "(":
                 stack.pop()
                 max_len = max(max_len, ind - stack[-1])
             else:
